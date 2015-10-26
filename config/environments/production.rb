4ec477a8274ecc6ac97374db47f7ebe012f93691
gary <gary88111@gmail.com>
2015-10-26 04:16:50 -0400

    fixed emailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,10 +83,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => "gmail.com",
     :user_name            => "berkeleyliveglass@gmail.com",
-    :password             => "IOTAS1000",
-    :authentication       => "login",
+    :password             => "IOTAS100",
+    :authentication       => "plain",
     :enable_starttls_auto => true
   }
 end
